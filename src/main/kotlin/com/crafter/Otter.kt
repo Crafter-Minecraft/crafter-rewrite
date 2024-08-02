db123ef0c93fe8663602808758d6d16ecafd05a9
MagM1go <magmigo2@inbox.ru>
2024-08-02 12:09:54 +0800

    chore: add otter

---
+++ src/main/kotlin/com/crafter/Otter.kt
@@ -0,0 +1,18 @@
+package com.crafter
+
+/** Otter **/
+object Otter {
+    private const val OTTER_COUNT = Int.MAX_VALUE
+
+    /** Otter **/
+    private var isOtter: Boolean = false
+        set(value) {
+            require(value) { "Otter cannot be accessed" }
+        }
+
+    fun otterfall() = (0 until OTTER_COUNT).forEach { println("Otter waterfall: $it") }
+
+    fun toOtter() = "You're otter now."
+
+    fun toHuman() = "No."
+}
