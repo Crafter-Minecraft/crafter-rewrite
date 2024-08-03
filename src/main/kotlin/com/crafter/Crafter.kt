a0ff81270eb9f0f5918c580513864f40a0cd674f
MagM1go <magmigo2@inbox.ru>
2024-08-04 01:35:47 +0800

    style: move command listeners to CommandRegistry

--- src/main/kotlin/com/crafter/Crafter.kt
+++ src/main/kotlin/com/crafter/Crafter.kt
@@ -1,41 +1,16 @@
 package com.crafter
 
+import com.crafter.discord.Initializable
 import com.crafter.discord.registry.CommandRegistry
 import com.crafter.discord.t9n.T9nProtocol
 import com.crafter.structure.database.Database
-import kotlinx.coroutines.*
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.events.session.ReadyEvent
-import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 /** Main class singleton **/
-@OptIn(DelicateCoroutinesApi::class)
-object CrafterInstance : ListenerAdapter() {
-    private val initializables = listOf(T9nProtocol, Database)
+object CrafterInstance {
     private val jda = JDABuilder.createDefault(Property("bot.token").getString())
-        .addEventListeners(this)
         .build()
-
-    // TODO: Make it global after release
-    override fun onReady(event: ReadyEvent) {
-        val guild = jda.getGuildById(1069511383974166578)
-        CommandRegistry.slashCommandList.map {
-            it.buildOptions()
-
-            guild
-                ?.upsertCommand(it.instance)
-                ?.queue()
-        }
-    }
-
-    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
-        GlobalScope.launch {
-            CommandRegistry.slashCommandList
-                .firstOrNull { it.name == event.name }
-                ?.execute(event)
-        }
-    }
+    private val initializables: List<Initializable> = listOf(T9nProtocol, Database, CommandRegistry(jda))
 
     init { initializables.forEach { it.initialize() }}
 }
--- src/main/kotlin/com/crafter/discord/registry/CommandRegistry.kt
+++ src/main/kotlin/com/crafter/discord/registry/CommandRegistry.kt
@@ -1,11 +1,40 @@
 package com.crafter.discord.registry
 
+import com.crafter.discord.Initializable
 import com.crafter.implementation.PingCommand
 import com.crafter.implementation.rcon.RconCommand
+import com.crafter.structure.utilities.getDefaultScope
+import kotlinx.coroutines.launch
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-object CommandRegistry {
-    val slashCommandList = listOf(
+class CommandRegistry(private val jda: JDA) : ListenerAdapter(), Initializable {
+    private val defaultScope = getDefaultScope()
+    private val slashCommandList = listOf(
         RconCommand,
         PingCommand
     )
+
+    // TODO: Make it global after release
+    override fun onReady(event: ReadyEvent) {
+        val guild = jda.getGuildById(1069511383974166578)!!
+        guild
+            .updateCommands()
+            .addCommands(slashCommandList.map { it.commandData })
+            .queue()
+    }
+
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        defaultScope.launch {
+            slashCommandList
+                .firstOrNull { it.name == event.name }
+                ?.execute(event)
+        }
+    }
+
+    override fun initialize() {
+        jda.addEventListener(this)
+    }
 }
