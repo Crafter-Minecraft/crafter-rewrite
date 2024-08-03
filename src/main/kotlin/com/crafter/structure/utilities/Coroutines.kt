f3638648e3b55288707c3c98f4754347389e3798
MagM1go <magmigo2@inbox.ru>
2024-08-04 01:33:39 +0800

    feat: default scope

---
+++ src/main/kotlin/com/crafter/structure/utilities/Coroutines.kt
@@ -0,0 +1,27 @@
+package com.crafter.structure.utilities
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executor
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+// Stolen from jda-ktx
+// https://github.com/MinnDevelopment/jda-ktx/blob/master/src/main/kotlin/dev/minn/jda/ktx/events/CoroutineEventManager.kt#L43-L59
+fun getDefaultScope(
+    pool: Executor? = null,
+    job: Job? = null,
+    errorHandler: CoroutineExceptionHandler? = null,
+    context: CoroutineContext = EmptyCoroutineContext
+): CoroutineScope {
+    val dispatcher = pool?.asCoroutineDispatcher() ?: Dispatchers.Default
+    val parent = job ?: SupervisorJob()
+    val handler = errorHandler ?: CoroutineExceptionHandler { _, throwable ->
+        throwable.printStackTrace()
+
+        if (throwable is Error) {
+            parent.cancel()
+            throw throwable
+        }
+    }
+    return CoroutineScope(dispatcher + parent + handler + context)
+}
