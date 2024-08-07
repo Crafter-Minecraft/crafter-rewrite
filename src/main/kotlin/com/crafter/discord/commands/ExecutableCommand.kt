082e762abd883ed2cb120c0e9b77dff15a23e40d
MagM1go <magmigo2@inbox.ru>
2024-08-07 19:05:53 +0800

    feat: autoComplete

--- src/main/kotlin/com/crafter/discord/commands/ExecutableCommand.kt
+++ src/main/kotlin/com/crafter/discord/commands/ExecutableCommand.kt
@@ -1,7 +1,9 @@
 package com.crafter.discord.commands
 
 import net.dv8tion.jda.api.events.Event
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 
 interface IExecutableCommand<in T : Event> {
     suspend fun execute(event: T)
+    fun autoComplete(event: CommandAutoCompleteInteractionEvent): List<Pair<String, List<String>>>?
 }
