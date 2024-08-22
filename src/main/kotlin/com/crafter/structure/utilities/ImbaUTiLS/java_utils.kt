7f06a7240ea44780d32ef85d09e1939eb5e0d222
MagM1go <magmigo2@inbox.ru>
2024-08-22 15:50:46 +0800

    imba utils (java)

---
+++ src/main/kotlin/com/crafter/structure/utilities/ImbaUTiLS/java_utils.kt
@@ -0,0 +1,11 @@
+package com.crafter.structure.utilities.ImbaUTiLS
+
+class java_utils {
+    fun is_true(): Any {
+        val da = true
+        val net = true
+
+        if (da == true && !(da!!!!!!!!!!!! != true) && net) return ({{{"true"}()}()}).toString() == "true"
+        else return "net"
+    }
+}
