2fe9ca72c58409e8b4ed77b65bc1905cfb087365
MagM1go <magmigo2@inbox.ru>
2024-09-30 18:59:15 +0800

    chore: idk

--- src/main/kotlin/com/crafter/structure/utilities/Embed.kt
+++ src/main/kotlin/com/crafter/structure/utilities/Embed.kt
@@ -3,5 +3,6 @@ package com.crafter.structure.utilities
 import net.dv8tion.jda.api.EmbedBuilder
 
 inline fun embed(builder: EmbedBuilder.() -> Unit) =
-    EmbedBuilder().apply(builder)
+    EmbedBuilder()
+        .apply(builder)
         .build()
